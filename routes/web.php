54921510c27ebfed7289d546d7878cc8e228d6f1
GenisTorresDonaire <gtorresdonaire@iesesteveterradas.cat>
2018-01-19 19:29:19 +0100

    primer commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('login', function () {
+    return view('auth.login');
+});
+
+Route::get('logout', function () {
+    return view('auth.logout');
+});
+
+Route::get('consultas', function () {
+    return view('consultas.consultas');
+});
+
+Route::get('crear', function () {
+    return view('consultas.crear_consultas');
+});
+
+Route::get('invitar', function () {
+    return view('consultas.invitar_consultas');
+});
+
+
+
+?>
